51458769765bb20e61912c9d672c96b8cb967d7e
Thilander Marcus (YISPC) <mxsu@eur.corp.vattenfall.com>
2026-08-19 22:00:08 +0200

    utbbokning api: add PATCH to CORS allow-methods

--- api/utbbokning/index.js
+++ api/utbbokning/index.js
@@ -22,7 +22,7 @@ const pool = new Pool({
 const corsHeaders = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS",
+  "Access-Control-Allow-Methods": "GET,POST,PATCH,DELETE,OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type"
 };
 
